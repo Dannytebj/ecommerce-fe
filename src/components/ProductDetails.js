1a7f2ad66f7be3461fdb0cd0824a068cdd50265e
Dannytebj <Dannytebj@gmail.com>
2019-05-18 11:49:09 +0100

    finish design product details

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getSingleProduct, getProductAtributes } from '../actions/productActions';
 
 class ProductDetails extends Component {
@@ -9,7 +10,8 @@ class ProductDetails extends Component {
       product: {},
       productAttribute: [],
       selectedSize: '',
-      selectedColor: ''
+      selectedColor: '',
+      quantity: 1
     }
   }
   componentWillReceiveProps(nextProps) {
@@ -18,37 +20,27 @@ class ProductDetails extends Component {
         product: nextProps.product,
         productAttribute: nextProps.productAttribute
       })
-      console.log("NextProps", nextProps);
     }
   }
-
-  // static getDerivedStateFromProps(props, state) {
-  //   if(props.product !== state.product) {
-  //     return {
-  //       product: props.product,
-  //       productAttribute: props.productAttribute
-  //     }
-  //   }
-  // }
 
   componentWillMount() {
     const { match: { params: { productId } } } = this.props;
     this.props.getSingleProduct(productId);
     this.props.getProductAtributes(productId)
-    console.log(productId);
   }
-  renderSizeAttrivalues = ({attribute_value_id, attribute_name, attribute_value }) => (
-    <li 
-      key={attribute_value_id} 
+  renderSizeAttrivalues = ({ attribute_value_id, attribute_name, attribute_value }) => (
+    <li
+      key={attribute_value_id}
       onClick={(e) => this.handleSizeSelect(e, attribute_value)}
-      className={(attribute_value === this.state.selectedSize) ? "list-group-item active"  : "list-group-item"}>
+      className={(attribute_value === this.state.selectedSize) ? "list-group-item active" : "list-group-item"}>
       {attribute_value}
     </li>
   );
-  renderColorAttrivalues = ({attribute_value_id, attribute_name, attribute_value }) => (
-    <li key={attribute_value_id} 
-    className={(attribute_value === this.state.selectedColor) ? "list-group-item active"  : "list-group-item"}>
-    {attribute_value}
+  renderColorAttrivalues = ({ attribute_value_id, attribute_name, attribute_value }) => (
+    <li key={attribute_value_id}
+      onClick={(e) => this.handleColorSelect(e, attribute_value)}
+      className={(attribute_value === this.state.selectedColor) ? "list-group-item active" : "list-group-item"}>
+      {attribute_value}
     </li>
   );
 
@@ -64,6 +56,25 @@ class ProductDetails extends Component {
       })
     }
 
+  }
+  handleColorSelect = (e, option) => {
+    e.preventDefault();
+    if (this.state.selectedColor === option) {
+      this.setState({
+        selectedColor: ''
+      })
+    } else {
+      this.setState({
+        selectedColor: option
+      })
+    }
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
   render() {
@@ -82,15 +93,24 @@ class ProductDetails extends Component {
       <div className="container-fluid product-details-wrapper">
         {(productAttribute.length > 0) ? <div className="card mb-3 product-details" >
           <div className="row no-gutters">
-            <div className="col-md-4">
+            <div className="col-md-3">
               <img src={require(`../styles/asset/${product.image}`)} className="card-img" alt="..." />
             </div>
-            <div className="col-md-8">
+            <div className="col-md-9">
               <div className="card-body">
+                <nav aria-label="breadcrumb">
+                  <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                    <li className="breadcrumb-item active" aria-current="page">{product.name}</li>
+                  </ol>
+                </nav>
                 <h3 className="card-title">{product.name}</h3>
-                <h1 className="card-title">{product.price}</h1>
-                <p className="card-text">{product.description}</p>
-                <div className="details-quantity"><h5>Quantity</h5><input type="number" /> </div>
+                <h1 className="card-title">${product.price}</h1>
+                <p className="card-text text-muted">{product.description}</p>
+                <div className="details-quantity">
+                  <h5>Quantity</h5>
+                  <input type="number" defaultValue={1} name="quantity" onChange={(e) => this.handleChange(e)} /> 
+                </div>
                 <div>
                   <h5>Sizes</h5>
                   <ul className="list-group list-group-horizontal">{(sizes.length > 0) ? renderedSizeList : ''}</ul>
@@ -99,7 +119,10 @@ class ProductDetails extends Component {
                   <h5>Colors</h5>
                   <ul className="list-group list-group-horizontal-md">{(colors.length > 0) ? renderedColorList : ''}</ul>
                 </div>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+
+                <div>
+                  <button className="btn btn-primary my-btn-1">Add to Cart</button>
+                </div>
               </div>
             </div>
           </div>
